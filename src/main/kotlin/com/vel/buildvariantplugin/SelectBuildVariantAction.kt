5badf29b19057812d3e1f124006472c17d7d593f
Velmurugan <velmuruganravi@fynd.team>
2024-04-10 11:55:15 +0530

    Fixed - Build variant name not updating when configuration changes

--- src/main/kotlin/com/vel/buildvariantplugin/SelectBuildVariantAction.kt
+++ src/main/kotlin/com/vel/buildvariantplugin/SelectBuildVariantAction.kt
@@ -1,29 +1,35 @@
 package com.vel.buildvariantplugin
 
 
+import com.android.tools.idea.gradle.model.variantNames
 import com.android.tools.idea.gradle.project.model.GradleAndroidModel
 import com.android.tools.idea.gradle.project.sync.GradleSyncState
 import com.android.tools.idea.gradle.variant.view.BuildVariantUpdater
 import com.android.tools.idea.projectsystem.getAndroidFacets
+import com.android.tools.idea.run.AndroidRunConfiguration
+import com.intellij.execution.RunManager
+import com.intellij.execution.RunManagerEx
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.actionSystem.ex.ComboBoxAction
-import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.IconLoader
 import icons.StudioIcons
+import org.jetbrains.android.facet.AndroidFacet
 import javax.swing.JComponent
 
 
-class SelectBuildVariantAction : ComboBoxAction(),DumbAware {
+class SelectBuildVariantAction : ComboBoxAction(), DumbAware {
 
     private val actions = ArrayList<AnAction>()
 
     override fun shouldShowDisabledActions(): Boolean {
         return false
     }
-    override fun createPopupActionGroup(button: JComponent,dataContext: DataContext): DefaultActionGroup {
+
+    override fun createPopupActionGroup(button: JComponent, dataContext: DataContext): DefaultActionGroup {
         val group = DefaultActionGroup()
         group.addAll(actions)
         return group
@@ -31,49 +37,61 @@ class SelectBuildVariantAction : ComboBoxAction(),DumbAware {
 
     override fun update(e: AnActionEvent) {
         e.presentation.isEnabled = false
-        var currentVariant = ""
-        e.project.getAndroidFacets().firstOrNull()?.configuration?.state?.SELECTED_BUILD_VARIANT?.let {
-            currentVariant = it
-            e.presentation.text = it
-        }
         e.presentation.icon = StudioIcons.Shell.ToolWindows.BUILD_VARIANTS
         e.presentation.disabledIcon = IconLoader.getDisabledIcon(StudioIcons.Shell.ToolWindows.BUILD_VARIANTS)
+        var currentVariant = ""
         val project = e.project
         if (project != null && !GradleSyncState.getInstance(project).isSyncInProgress) {
-            val moduleManager = ModuleManager.getInstance(project)
-            val androidModules = moduleManager.modules
-                .map { GradleAndroidModel.get(it) }
-                .filter { it?.moduleName != null }
-                .map { it!! }
-                .distinct()
-            getBuildTypes(androidModules.firstOrNull()?.variantNames,project,currentVariant)
-            e.presentation.isEnabled = actions.isNotEmpty()
-        }else{
+            val runManager = RunManager.getInstance(project) as RunManagerEx
+            val selectedConfiguration = runManager.selectedConfiguration
+            val runConfiguration = selectedConfiguration?.configuration
+            if (runConfiguration is AndroidRunConfiguration) {
+                val module = runConfiguration.modules.firstOrNull()
+                val androidFacet = module?.let { AndroidFacet.getInstance(it) }
+                val androidModule = module?.let { GradleAndroidModel.get(it) }
+                if (androidFacet != null && androidModule != null) {
+                    androidFacet.configuration.state.SELECTED_BUILD_VARIANT?.let {
+                        currentVariant = it
+                        e.presentation.text = it
+                    }
+                    getBuildTypes(androidModule.androidProject.variantNames, project, currentVariant, module)
+                    e.presentation.isEnabled = actions.isNotEmpty()
+                } else {
+                    actions.clear()
+                }
+            } else {
+                actions.clear()
+            }
+        } else {
             actions.clear()
-            e.presentation.isEnabled = false
         }
         super.update(e)
     }
 
-    private fun getBuildTypes(buildVariants: Collection<String>?, project: Project, currentVariant: String){
+    private fun getBuildTypes(
+        buildVariants: Collection<String>?,
+        project: Project,
+        currentVariant: String,
+        module: Module
+    ) {
         actions.clear()
         buildVariants?.forEach { variant ->
             actions.add(object : AnAction(variant) {
                 override fun update(e: AnActionEvent) {
                     e.presentation.text = variant
-                    if(currentVariant == variant){
+                    if (currentVariant == variant) {
                         e.presentation.icon = AllIcons.Actions.Checked_selected
-                    }else{
+                    } else {
                         e.presentation.icon = null
                     }
                     super.update(e)
                 }
 
                 override fun actionPerformed(e: AnActionEvent) {
-                    if(e.presentation.text != currentVariant) {
+                    if (e.presentation.text != currentVariant) {
                         project.getAndroidFacets().firstOrNull()?.mainModule?.let {
                             BuildVariantUpdater.getInstance(project).run {
-                                updateSelectedBuildVariant(it, variant)
+                                updateSelectedBuildVariant(module, variant)
                             }
                         }
                     }
